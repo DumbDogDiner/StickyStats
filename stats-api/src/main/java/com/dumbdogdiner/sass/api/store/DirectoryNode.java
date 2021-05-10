0be476f035fd2965e2dfd2a4428087e7c183d207
Matt <25674682+spazzylemons@users.noreply.github.com>
2021-05-10 18:17:00 -0400

    Fix errors with hierarchy for statistics

--- stats-api/src/main/java/com/dumbdogdiner/sass/api/store/DirectoryNode.java
+++ stats-api/src/main/java/com/dumbdogdiner/sass/api/store/DirectoryNode.java
@@ -3,4 +3,4 @@ package com.dumbdogdiner.sass.api.store;
 /**
  * Represents a node that contains other nodes, and possibly statistics.
  */
-public interface DirectoryNode extends Node<DirectoryNode> {}
+public interface DirectoryNode extends DirectoryStorableNode<DirectoryStorableNode<?>> {}
--- stats-api/src/main/java/com/dumbdogdiner/sass/api/store/statistic/Statistic.java
+++ stats-api/src/main/java/com/dumbdogdiner/sass/api/store/statistic/Statistic.java
@@ -2,15 +2,15 @@ package com.dumbdogdiner.sass.api.store.statistic;
 
 import java.util.UUID;
 
+import com.dumbdogdiner.sass.api.store.DirectoryStorableNode;
 import com.dumbdogdiner.sass.api.store.NeverNode;
-import com.dumbdogdiner.sass.api.store.Node;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents a terminal node containing a statistic.
  * @param <T> The type of statistic this node contains.
  */
-public interface Statistic<T> extends Node<NeverNode> {
+public interface Statistic<T> extends DirectoryStorableNode<NeverNode> {
     /**
      * @param playerId The {@link UUID} of the player.
      * @return The value of the statistic for a player, or the initial value if it does not exist.
