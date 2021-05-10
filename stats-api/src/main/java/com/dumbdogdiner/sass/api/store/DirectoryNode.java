2b01aa9f97c8a868dd84039810515512ab16a6b3
Matt <25674682+spazzylemons@users.noreply.github.com>
2021-05-10 18:32:00 -0400

    ok here's the implementation that I mentioned in that last commit

--- stats-api/src/main/java/com/dumbdogdiner/sass/api/store/DirectoryNode.java
+++ stats-api/src/main/java/com/dumbdogdiner/sass/api/store/DirectoryNode.java
@@ -1,6 +1,16 @@
 package com.dumbdogdiner.sass.api.store;
 
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Represents a node that contains other nodes, and possibly statistics.
  */
-public interface DirectoryNode extends DirectoryStorableNode<DirectoryStorableNode<?>> {}
+public interface DirectoryNode extends DirectoryStorableNode<DirectoryStorableNode<?>> {
+    /**
+     * @param id The identifier of the new DirectoryNode.
+     * @param plugin The plugin creating the new DirectoryNode.
+     * @return A new DirectoryNode which is a child of this node.
+     */
+    DirectoryNode mkdir(@NotNull String id, @NotNull JavaPlugin plugin);
+}
--- stats-plugin/src/main/kotlin/com/dumbdogdiner/sass/stats/impl/store/DirectoryNodeImpl.kt
+++ stats-plugin/src/main/kotlin/com/dumbdogdiner/sass/stats/impl/store/DirectoryNodeImpl.kt
@@ -1,27 +1,31 @@
 package com.dumbdogdiner.sass.stats.impl.store
 
 import com.dumbdogdiner.sass.api.store.DirectoryNode
-import com.dumbdogdiner.sass.api.store.Node
 import org.bukkit.plugin.java.JavaPlugin
 
-class DirectoryNodeImpl : DirectoryNode {
-    override fun getIdentifier(): String {
-        TODO("Not yet implemented")
-    }
+// TODO database integration
+class DirectoryNodeImpl private constructor(
+    private val id: String,
+    private val plugin: JavaPlugin,
+    private val parent: DirectoryNode,
+) : DirectoryNode {
+    private val children = mutableMapOf<String, DirectoryNode>()
 
-    override fun getPlugin(): JavaPlugin {
-        TODO("Not yet implemented")
-    }
+    override fun getIdentifier() = id
 
-    override fun getParent(): Node<*> {
-        TODO("Not yet implemented")
-    }
+    override fun getPlugin() = plugin
 
-    override fun getChildren(): Array<DirectoryNode> {
-        TODO("Not yet implemented")
-    }
+    override fun getParent() = parent
 
-    override fun get(id: String): DirectoryNode? {
-        TODO("Not yet implemented")
+    override fun getChildren() = children.values.toTypedArray()
+
+    override operator fun get(id: String) = children[id]
+
+    override fun mkdir(id: String, plugin: JavaPlugin): DirectoryNode {
+        val dir = DirectoryNodeImpl(id, plugin, this)
+        // add the child to our children list
+        children[id] = dir
+        // return the new directory
+        return dir
     }
 }
