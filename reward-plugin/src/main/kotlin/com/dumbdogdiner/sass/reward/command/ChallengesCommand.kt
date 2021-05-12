68cd5dadcd52390cc28b4d1b23b5f013225d6aa1
Matt <25674682+spazzylemons@users.noreply.github.com>
2021-05-12 11:42:42 -0400

    Coerce progress in challenges command

--- reward-plugin/src/main/kotlin/com/dumbdogdiner/sass/reward/command/ChallengesCommand.kt
+++ reward-plugin/src/main/kotlin/com/dumbdogdiner/sass/reward/command/ChallengesCommand.kt
@@ -19,10 +19,13 @@ object ChallengesCommand {
                     sender.sendMessage(challenge.name)
                     val reward = challenge.reward.apply(uuid)
                     if (reward > 0) {
+                        val percentage = floor(challenge.progress.apply(uuid) * 100).toInt().coerceIn(0..99)
+                        val progressString = challenge.progressString.apply(uuid)
+                        val goalString = challenge.goalString.apply(uuid)
                         sender.sendMessage("  Reward: $reward Miles")
-                        sender.sendMessage("  Completion: ${floor(challenge.progress.apply(uuid) * 100).toInt()}%")
-                        sender.sendMessage("  Progress: ${challenge.progressString.apply(uuid)}")
-                        sender.sendMessage("  Goal: ${challenge.goalString.apply(uuid)}")
+                        sender.sendMessage("  Completion: $percentage%")
+                        sender.sendMessage("  Progress: $progressString")
+                        sender.sendMessage("  Goal: $goalString")
                     } else {
                         sender.sendMessage("  Unattainable")
                     }
