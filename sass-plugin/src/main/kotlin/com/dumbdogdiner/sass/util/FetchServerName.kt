bfa59b7c73134c6d6c21104188ab7a0bcc586155
Matt <25674682+spazzylemons@users.noreply.github.com>
2021-05-15 20:12:25 -0400

    Don't block for response from BungeeCord

--- sass-plugin/src/main/kotlin/com/dumbdogdiner/sass/util/FetchServerName.kt
+++ sass-plugin/src/main/kotlin/com/dumbdogdiner/sass/util/FetchServerName.kt
@@ -12,47 +12,41 @@ private const val BUNGEECORD_CHANNEL = "BungeeCord"
 /**
  * Gets the server name by communicating with BungeeCord.
  */
-private fun fetchServerNameViaBungee(): String {
-    // create channel to transmit result
-    val result = Channel<String>()
+private fun fetchServerNameViaBungee() {
     // create listener
-    val listener = PluginMessageListener listener@{ channel, _, message ->
-        // must be on bungee channel
-        if (channel != BUNGEECORD_CHANNEL) return@listener
-        // must be GetServer message
-        @Suppress("UnstableApiUsage")
-        val stream = ByteStreams.newDataInput(message)
-        if (stream.readUTF() != "GetServer") return@listener
-        // get server name
-        val serverName = stream.readUTF()
-        runBlocking {
-            result.send(serverName)
+    var listener = null as PluginMessageListener?
+    listener = PluginMessageListener listener@{ channel, _, message ->
+        try {
+            // must be on bungee channel
+            if (channel != BUNGEECORD_CHANNEL) return@listener
+            // must be GetServer message
+            @Suppress("UnstableApiUsage")
+            val stream = ByteStreams.newDataInput(message)
+            if (stream.readUTF() != "GetServer") return@listener
+            // get server name
+            SassPlugin.instance.serverName = stream.readUTF()
+        } finally {
+            // unregister the listener
+            SassPlugin.instance.server.messenger.also { messenger ->
+                messenger.unregisterIncomingPluginChannel(SassPlugin.instance, BUNGEECORD_CHANNEL, listener!!)
+                messenger.unregisterOutgoingPluginChannel(SassPlugin.instance, BUNGEECORD_CHANNEL)
+            }
         }
     }
-    try {
-        // register the listener
-        SassPlugin.instance.server.messenger.also { messenger ->
-            messenger.registerIncomingPluginChannel(SassPlugin.instance, BUNGEECORD_CHANNEL, listener)
-            messenger.registerOutgoingPluginChannel(SassPlugin.instance, BUNGEECORD_CHANNEL)
-        }
-        // send GetServer message
-        @Suppress("UnstableApiUsage")
-        val stream = ByteStreams.newDataOutput()
-        stream.writeUTF("GetServer")
-        SassPlugin.instance.server.sendPluginMessage(SassPlugin.instance, BUNGEECORD_CHANNEL, stream.toByteArray())
-        // return response
-        return runBlocking { result.receive() }
-    } finally {
-        // unregister the listener
-        SassPlugin.instance.server.messenger.also { messenger ->
-            messenger.unregisterIncomingPluginChannel(SassPlugin.instance, BUNGEECORD_CHANNEL, listener)
-            messenger.unregisterOutgoingPluginChannel(SassPlugin.instance, BUNGEECORD_CHANNEL)
-        }
+    // register the listener
+    SassPlugin.instance.server.messenger.also { messenger ->
+        messenger.registerIncomingPluginChannel(SassPlugin.instance, BUNGEECORD_CHANNEL, listener)
+        messenger.registerOutgoingPluginChannel(SassPlugin.instance, BUNGEECORD_CHANNEL)
     }
+    // send GetServer message
+    @Suppress("UnstableApiUsage")
+    val stream = ByteStreams.newDataOutput()
+    stream.writeUTF("GetServer")
+    SassPlugin.instance.server.sendPluginMessage(SassPlugin.instance, BUNGEECORD_CHANNEL, stream.toByteArray())
 }
 
 fun fetchServerName() {
     if (Bukkit.spigot().spigotConfig.getBoolean("settings.bungeecord", false)) {
-        SassPlugin.instance.serverName = fetchServerNameViaBungee()
+        fetchServerNameViaBungee()
     }
 }
