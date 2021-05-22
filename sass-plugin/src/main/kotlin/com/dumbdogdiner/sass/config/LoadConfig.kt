0eeae5e0dab0394eb57ae93bacfdf87b1c111c8a
Matt <25674682+spazzylemons@users.noreply.github.com>
2021-05-22 19:24:07 -0400

    Adding more stuff to config, still no implementation yet

--- sass-plugin/src/main/kotlin/com/dumbdogdiner/sass/config/LoadConfig.kt
+++ sass-plugin/src/main/kotlin/com/dumbdogdiner/sass/config/LoadConfig.kt
@@ -1,15 +1,20 @@
 package com.dumbdogdiner.sass.config
 
+import com.dumbdogdiner.sass.SassPlugin
 import com.dumbdogdiner.sass.api.stats.Statistic
 import com.dumbdogdiner.sass.impl.stats.StatisticImpl
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.EntityType
 import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
+import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.entity.EntityDeathEvent
 import java.util.UUID
-
-// entities killed { mob type }
 
 private abstract class GoalSpec(val name: String, vararg val parameters: String) {
     abstract fun createListener(stat: StatisticImpl, parameters: Array<String>): GoalHandler<*>
@@ -21,10 +26,18 @@ private fun Statistic.increment(playerId: UUID) {
 }
 
 private abstract class GoalHandler<T : Event> : Listener {
-    abstract val eventType: Class<T>
+    abstract val handlerList: HandlerList
 
     @EventHandler
     abstract fun onEvent(event: T)
+
+    fun register() {
+        Bukkit.getPluginManager().registerEvents(this, SassPlugin.instance)
+    }
+
+    fun unregister() {
+        handlerList.unregister(this)
+    }
 }
 
 private val goalSpecs = arrayOf(
@@ -33,7 +46,7 @@ private val goalSpecs = arrayOf(
             val mobType = EntityType.valueOf(parameters[0])
 
             return object : GoalHandler<EntityDeathEvent>() {
-                override val eventType = EntityDeathEvent::class.java
+                override val handlerList = EntityDeathEvent.getHandlerList()
 
                 override fun onEvent(event: EntityDeathEvent) {
                     if (event.entity.type != mobType) return
@@ -42,5 +55,43 @@ private val goalSpecs = arrayOf(
                 }
             }
         }
+    },
+
+    object : GoalSpec("blocks mined", "block") {
+        override fun createListener(stat: StatisticImpl, parameters: Array<String>): GoalHandler<*> {
+            // TODO warn if block drops itself?
+            val material = Material.valueOf(parameters[0])
+
+            return object : GoalHandler<BlockBreakEvent>() {
+                override val handlerList = BlockBreakEvent.getHandlerList()
+
+                override fun onEvent(event: BlockBreakEvent) {
+                    if (event.block.type != material) return
+                    val activeItem = event.player.activeItem ?: return
+                    if (Enchantment.SILK_TOUCH in activeItem.enchantments) return
+                    stat.increment(event.player.uniqueId)
+                }
+            }
+        }
     }
-)
+).associateBy(GoalSpec::name)
+
+private val sampleConfig = YamlConfiguration().apply {
+    load(
+"""
+challenges:
+- name: Kill Skeletons
+  stat name: kill skeletons
+  goal:
+    name: entities killed
+    mob type: SKELETON
+  tiers:
+  - threshold: 10
+    reward: 10
+  - threshold: 30
+    reward: 30
+  - threshold: 50
+    reward: 50
+"""
+    )
+}
